4149e08a60c36ef64a5dd4b1b3fd21072cbe4421
Kim-An Quinn <kiman.quinn@bc.edu>
2019-09-02 12:54:41 -0400

    2.5 completed: second button added. Shows two messages with different colors

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
         messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.red
     }
     
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are great!"
+        messageLabel.textColor = UIColor.blue
+    }
 }
 
