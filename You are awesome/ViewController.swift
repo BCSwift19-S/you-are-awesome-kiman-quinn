f1f7e7290c837991240cfefd63ab5424c15ed35f
Kim-An Quinn <kiman.quinn@bc.edu>
2019-09-08 13:24:48 -0400

    2.18 completed: added functions for showing a message, showing an image, and playing a sound

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -24,48 +24,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int{
+        var newIndex: Int
+        repeat{
+            newIndex = Int.random(in: 0..<maxValue)
+        }while lastNumber == newIndex
+         return newIndex
+    }
 
-    @IBAction func showMessagePressed(_ sender: UIButton) {
-        
-        let messages = ["You are awesome!",
-                        "You are great!",
-                        "You are fantastic!",
-                        "When the Genius Bar needs help, they call you!",
-                        "You brighten my day!",
-                        "You are da bomb!",
-                        "Hey, fabulous!",
-                        "You are tremendous!",
-                        "You've got the design skills of Jony Ive!",
-                        "I can't wait to download your app!"]
-
-        var newIndex: Int //declares but doesn't initialize, explicit
-        
-        //Show a message
-        repeat{
-            newIndex = Int.random(in: 0..<messages.count)
-        }while index == newIndex
-        
-        index = newIndex
-        messageLabel.text = messages[index]
-        
-        //Show an image
-        repeat{
-            newIndex = Int.random(in: 0..<numberOfImages)
-        }while imageIndex == newIndex
-        
-        imageIndex = newIndex
-        
-        awesomeimageView.image = UIImage(named: "image\(imageIndex)")
-        
-        //Get a random number to use for our sound name file
-        repeat{
-            newIndex = Int.random(in: 0..<numberOfSounds)
-        }while soundIndex == newIndex
-        
-        soundIndex = newIndex
-        
-        //Play a sound
-        var soundName = "sound\(soundIndex)"
+    func playSound(soundName: String){
         if let sound = NSDataAsset(name: soundName){
             //check if sound.data is a sound file
             do{
@@ -79,6 +47,36 @@ class ViewController: UIViewController {
             //if reading in the NSDataAsset didn't work, tell the developer/report the error.
             print("ERROR: file\(soundName) didn't load")
         }
+    }
+    
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        
+        let messages = ["You are awesome!",
+                        "You are great!",
+                        "You are fantastic!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You brighten my day!",
+                        "You are da bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Jony Ive!",
+                        "I can't wait to download your app!"]
+
+        
+        //Show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
+        messageLabel.text = messages[index]
+
+        //Show an image
+        imageIndex = nonRepeatingRandom(lastNumber: imageIndex, maxValue: numberOfImages)
+        awesomeimageView.image = UIImage(named: "image\(imageIndex)")
+        
+        //Get a random number to use for our sound name file
+        soundIndex = nonRepeatingRandom(lastNumber: soundIndex, maxValue: numberOfSounds)
+        
+        //Play sound
+        let soundName = "sound\(soundIndex)"
+        playSound(soundName: soundName)
 
     }
     
