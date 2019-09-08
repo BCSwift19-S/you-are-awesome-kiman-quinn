3f7a4474f87b3b1cea3b3a01d27a1161b22680c1
Kim-An Quinn <kiman.quinn@bc.edu>
2019-09-08 13:39:59 -0400

    2.19 completed: added parameters to functions

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -33,12 +33,12 @@ class ViewController: UIViewController {
          return newIndex
     }
 
-    func playSound(soundName: String){
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer){
         if let sound = NSDataAsset(name: soundName){
             //check if sound.data is a sound file
             do{
-                try awesomePlayer = AVAudioPlayer(data: sound.data)
-                awesomePlayer.play()
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
             }catch{
                 //if sound.data is not a valid audio file
                 print("ERROR: data in \(soundName) couldn't be played as a sound")
@@ -76,7 +76,7 @@ class ViewController: UIViewController {
         
         //Play sound
         let soundName = "sound\(soundIndex)"
-        playSound(soundName: soundName)
+        playSound(soundName: soundName, audioPlayer: &awesomePlayer)
 
     }
     
